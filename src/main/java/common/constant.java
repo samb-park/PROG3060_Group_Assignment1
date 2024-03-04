d74ba635ca9e5d914b4bbd363f7c1b8b9f7b6949
Sangbong Park <sangbong.park@outlook.com>
2024-03-03 20:30:34 -0500

    Added username and password

--- src/main/java/common/constant.java
+++ src/main/java/common/constant.java
@@ -2,4 +2,6 @@ package common;
 
 public class constant {
     public static final int RegisterSuccess = 1;
+    public static final String Username = "admin";
+    public static final String Password = "admin";
 }
